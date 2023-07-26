860d5fb5527cc8f8f85006c68a12f84ddb28c140
vivienrauch <vivienmirjam@gmail.com>
2023-07-26 09:23:50 +0000

    installations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <NavBar /> 
     </div>
   );
 }
