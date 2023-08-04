b103ec081839f37823b1e70da36c2c0badf75661
vivienrauch <vivienmirjam@gmail.com>
2023-08-04 11:04:24 +0000

    save changes

--- src/api/axiosDefault.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = 'https://drf-api-vmr-c9fd534dbc4b.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true;
